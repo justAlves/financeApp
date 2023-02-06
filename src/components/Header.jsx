1d83f15dc9f5e88368e7f6d9dad68750dd2000df
justAlves <iamalves.jpg@gmail.com>
2023-02-05 21:59:06 -0300

    Updates

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default props => {
+    <div className="flex w-screen h-1/6 bg-[#c9184a]">
+
+    </div>
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header'
+
+export default props => {
+    return(
+        <div className="flex h-screen w-screen justify-center items-center bg-[#fff0f3]">
+            <Header/>
+        </div>
+    )
+}
